3a85056fd23269c375c5f29b3b0368a2fbf996ad
Artem Akilov <bfvniiaes-aas@yandex.ru>
2024-02-23 14:20:51 +0500

    commit_2

--- tests.py
+++ tests.py
@@ -14,13 +14,14 @@ class TestBooksCollector:
     @ pytest.mark.parametrize('book_name', names)
     def test_add_new_book_add_new_book(self, collector, book_name):
         collector.add_new_book(book_name)
-        assert len(collector.books_genre) == 1
+        assert book_name in collector.get_books_genre(), (f'Книга "{book_name}" не добавлена в список! Длина названия'
+                                                          f' - {len(book_name)}.')
 
     # test_002 - негативный, добавление книги, имеющейся в словаре (длина названия 1-40 символов - 18 символов)
     def test_add_new_book_add_same_book(self, collector):
         collector.add_new_book('А зори здесь тихие')
         collector.add_new_book('А зори здесь тихие')
-        assert len(collector.books_genre) == 1
+        assert len(collector.get_books_genre()) == 1
 
     # test_003 - негативный, добавление новой книги с некорректной длиной названия (длина названия НЕ 1-40 символов - 0,
     #  41, 42, 61 символов)
@@ -33,25 +34,26 @@ class TestBooksCollector:
     @ pytest.mark.parametrize('book_name', names)
     def test_add_new_book_add_book_with_incorrect_name(self, collector, book_name):
         collector.add_new_book(book_name)
-        assert len(collector.books_genre) == 0
+        assert book_name not in collector.get_books_genre(), (f'Книга "{book_name}" не добавлена в список! Длина '
+                                                              f'названия - {len(book_name)}.')
 
     # test_004 - позитивнный, установление книге жанра (книга из словаря, жанр из словаря)
     def test_set_book_genre_set_book_genre(self, collector):
         collector.add_new_book('Колобок')
         collector.set_book_genre('Колобок', 'Фантастика')
-        assert collector.books_genre['Колобок'] == 'Фантастика'
+        assert collector.get_book_genre('Колобок') == 'Фантастика'
 
     # test_005 - негативный, установление книге жанра (книга НЕ из словаря, жанр из словаря)
     def test_set_book_genre_incorrect_name_book(self, collector):
         collector.add_new_book('Колобок')
         collector.set_book_genre('Репка', 'Ужасы')
-        assert 'Репка' not in collector.books_genre
+        assert 'Репка' not in collector.get_books_genre()
 
     # test_006 - негативный, установление книге жанра (книга из словаря, жанр НЕ из словаря)
     def test_set_book_genre_incorrect_name_genre(self, collector):
         collector.add_new_book('Колобок')
         collector.set_book_genre('Колобок', 'Аудио')
-        assert collector.books_genre['Колобок'] == ''
+        assert collector.get_book_genre('Колобок') == ''
 
     # test_007 - возврат жанра книги по имени (набор 1 - позитивный - название книги из словаря, набор 2 - негативный -
     # название книги НЕ из словаря)
@@ -73,7 +75,8 @@ class TestBooksCollector:
         collector.set_book_genre('Чук и Гек', 'Ужасы')
         collector.set_book_genre('Азбука', 'Фантастика')
         collector.set_book_genre('Сказки', 'Ужасы')
-        assert len(collector.get_books_with_specific_genre('Ужасы')) == 2
+        assert ('Чук и Гек', 'Сказки' in collector.get_books_with_specific_genre('Ужасы')
+                             and len(collector.get_books_with_specific_genre('Ужасы')) == 2)
 
     # test_009 - негативный - возврат списка книг определенного жанра (словарь книг пустой)
     def test_get_books_with_specific_genre_no_books(self, collector):
@@ -89,23 +92,18 @@ class TestBooksCollector:
 
     # test_011 - позитивный - возврат списка книг
     def test_get_books_genre_return_books_genre(self, collector):
-        collector.add_new_book('Айболит')
-        collector.set_book_genre('Айболит', 'Мультфильмы')
         collector.add_new_book('Солярис')
-        collector.set_book_genre('Солярис', 'Фантастика')
-        assert (collector.get_books_genre()['Айболит'] == 'Мультфильмы' and collector.get_books_genre()['Солярис'] ==
-                'Фантастика')
+        assert 'Солярис' in collector.get_books_genre() and len(collector.get_books_genre()) == 1
 
     # test_012 - позитивный - возврат списка книг, подходящим детям
     def test_get_books_for_children_return_books_for_children(self, collector):
-        collector.add_new_book('Крошка Енот')
-        collector.set_book_genre('Крошка Енот', 'Мультфильмы')
         collector.add_new_book('Черная комната')
         collector.set_book_genre('Черная комната', 'Ужасы')
         collector.add_new_book('Красная шапочка')
         collector.set_book_genre('Красная шапочка', 'Комедии')
         assert ('Черная комната' not in collector.get_books_for_children()
-                and 'Крошка Енот', 'Красная шапочка' in collector.get_books_for_children())
+                and 'Красная шапочка' in collector.get_books_for_children()
+                and len(collector.get_books_for_children()) == 1)
 
     # test_013 - добавление книги в избранное (набор 1 позитивный - название книги из словаря, в избранном отсутствует,
     # набор 2 негативный - название книги НЕ из словаря, в избранном отсутвует, набор 3 негативный - название книги
@@ -121,7 +119,7 @@ class TestBooksCollector:
         collector.add_new_book(book_2)
         collector.add_book_in_favorites(book_1)
         collector.add_book_in_favorites(book_3)
-        assert len(collector.favorites) == result
+        assert len(collector.get_list_of_favorites_books()) == result
 
     # test_014 - удаление книги из избранного (набор 1 - позитивный - название книги в изюранном присутствует,
     # набор 2 - негативный - название книги в избранном отсутвует)
@@ -134,15 +132,16 @@ class TestBooksCollector:
         collector.add_new_book(book_name_1)
         collector.add_book_in_favorites(book_name_1)
         collector.delete_book_from_favorites(book_name_2)
-        assert len(collector.favorites) == result
+        assert len(collector.get_list_of_favorites_books()) == result
 
     # test_015 - позитивный - возврат списка избранных книг
     def test_get_list_of_favorites_books_return_list_of_favorites_books(self, collector):
         collector.add_new_book('Дэнс дэнс дэнс')
         collector.add_book_in_favorites('Дэнс дэнс дэнс')
-        assert collector.get_list_of_favorites_books() == ['Дэнс дэнс дэнс']
+        assert ('Дэнс дэнс дэнс' in collector.get_list_of_favorites_books()
+                and len(collector.get_list_of_favorites_books()) == 1)
 
-    # test_016 - позитивный - у добавленной книги нет жанра
+    # test_016 - позитивный - у вновь добавленной книги не установлен жанр
     def test_add_new_book_no_genre(self, collector):
         collector.add_new_book('Проверка микрофона')
-        assert collector.books_genre['Проверка микрофона'] == ''
+        assert collector.get_book_genre('Проверка микрофона') == ''
